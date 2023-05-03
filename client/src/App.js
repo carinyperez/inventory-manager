b41cf5b6be66284bd47e5fca84403bbacc04511e
Cariny <perezcariny@gmail.com>
2023-05-03 10:35:23 -0700

    feat: add homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
+import logo from './tim_logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      </header>
+		<img src={logo} alt='logo'/>
+		<a href='/store'>Preview your store</a>
+		<a href='/inventory'>Manage your inventory</a>
+	  </header>
+	  <Router>
+		<Routes>
+			<Route path='/' element={<HomePage/>} ></Route>
+		</Routes>
+	  </Router>
     </div>
   );
 }
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import './HomePage.css';
+
+const HomePage = () => {
+	return (
+		<main>
+			<h1>The Inventory Manager</h1>
+			<p>Manage your store inventory and preview your store all in one place</p>
+			<img src="/background.jpg" alt='background' ></img>
+		</main>
+	)
+}
+
+export default HomePage; 
+
